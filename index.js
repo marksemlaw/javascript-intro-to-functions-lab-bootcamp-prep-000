e1975a7f7c322a8c7e4e14419b7ad06764c8c1fb
Mark Semotiuk <marksem@gmail.com>
2018-07-17 00:27:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function logShout (string) {
   console.log(string.toUpperCase())
 }
 
-function sayHiToGrandma(random) {
-  if (random === "hello!") {
+function sayHiToGrandma(string) {
+  if (string === "hello!") {
     return("I can't hear you!")
     }
     else if (string === "HELLO!") {
