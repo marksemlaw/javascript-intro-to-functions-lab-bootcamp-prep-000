8638cdd1cd6875086ecc1849f14cb51a149181e7
Mark Semotiuk <marksem@gmail.com>
2018-07-17 00:28:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logShout (string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === "hello!") {
+  if (string === "hello") {
     return("I can't hear you!")
     }
-    else if (string === "HELLO!") {
+    else if (string === "HELLO") {
     return("YES INDEED!")
     }
     else if (string === "I love you, Grandma.") {
